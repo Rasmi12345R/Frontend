07dafc0c6d2b18193d03b539e08bb0b262b9efd2
= <rasmitrikhatri@gmail.com>
2025-01-03 13:32:11 +0545

    Project add

---
+++ Project/project5 navbar/navbar.js
@@ -0,0 +1,8 @@
+//javascript to toggle menu
+const nepal = document.getElementById('optionBar')
+const menu = document.getElementById('menu')
+
+nepal.addEventListener("click",()=>{
+    menu.classList.toggle("data");
+});
+
